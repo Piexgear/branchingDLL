9d8628169f89c64fa8e81ffca784a90373db15de
David Puscas <davidpuscas@live.se>
2025-10-06 11:46:49 +0200

    added mainhand
    added mainhand weapon in class user class.

--- userClass.cs
+++ userClass.cs
@@ -4,11 +4,13 @@ class User
 {
     public string Username;
     public int HP; //health point
+    public Weapon MainHand;
 
 
-    public User(string Username, int hp)
+    public User(string Username, int hp, Weapon mainhad)
     {
         Username = Username;
         HP = hp;
+        MainHand = mainhad;
     }
 }
