62b19ee05a011dac5a848543620a66563f948cd8
linahalle <hallergren.lina@gmail.com>
2025-10-06 11:28:36 +0200

    Created the user-class

---
+++ userClass.cs
@@ -0,0 +1,14 @@
+namespace App;
+
+class User
+{
+    public string Username;
+    public int HP; //health point
+
+
+    public User(string Username, int hp)
+    {
+        Username = Username;
+        HP = hp;
+    }
+}
